13cc42d32be53bbedfe3c49fa377e09f7a1ce856
farazMehraien95 <farazmehraien95@gmail.com>
2023-10-13 22:15:22 +0330

    add rewrite api

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://172.16.30.50:8086/'
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
