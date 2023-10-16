67d2818415d32c5b11166f2de518f256d485317f
farazMehraien95 <farazmehraien95@gmail.com>
2023-10-16 21:53:03 +0330

    add custom font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,20 @@
 'use client'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 import UserProvider from '../context/UserContext'
-const inter = Inter({ subsets: ['latin'] })
 
+const iransansFont = localFont({ src: '../public/fonts/iransans/fonts.ttf' })
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>
+    <html lang='en' className={iransansFont.className}>
+      <body>
+        <style jsx global>{`
+          :root {
+            /* ... */
+            --iransans-font: ${iransansFont.style.fontFamily};
+          }
+        `}</style>
+
         <UserProvider>{children}</UserProvider>
       </body>
     </html>
